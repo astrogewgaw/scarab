9d62c4411982ff7f00e2cb090988c002c4b04a51
Ujjwal Panda <ujjwalpanda97@gmail.com>
2025-01-09 01:47:59 +0530

    🐛 Fix bug in docs build.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,11 +1,15 @@
 project = "scarab"
 html_theme = "furo"
+master_doc = "index"
 author = "Ujjwal Panda"
-master_doc = "contents"
 exclude_patterns = [""]
 copyright = "2021-2022, Ujjwal Panda"
 html_baseurl = "https://scarab.readthedocs.io"
 
+source_suffix = {
+    ".md": "markdown",
+    ".rst": "restructuredtext",
+}
 
 extensions = extensions = [
     "nbsphinx",
